25cb11e84c3471d07324ae339c0fef3f3d305e11
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-10 12:40:05 +0900

    Add : Klayswap Header Design
    Klayswap Header Design

---
+++ DAPP/client/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
