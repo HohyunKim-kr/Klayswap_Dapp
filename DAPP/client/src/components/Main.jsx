13a1e1b13162889d9c5bb3443e173f5f44b0755b
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-15 23:00:21 +0900

    Update Main.jsx
    add Address context

--- DAPP/client/src/components/Main.jsx
+++ DAPP/client/src/components/Main.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SwapContext } from "../context/SwapProvider";
 import "./Main.css";
 
 const Main = () => {
@@ -11,11 +12,12 @@ const Main = () => {
 };
 
 const Title = () => {
+  const { myAddress } = useContext(SwapContext);
   return (
     <section>
       <span className="main-text">
         원하는 자산으로 바로 <span className="text-strong">교환(스왑)</span>
-        하세요
+        하세요{myAddress}
       </span>
     </section>
   );
@@ -27,7 +29,10 @@ const Ticket = () => {
       <section>
         <KlayCardItem />
         <article className="change-icon">
-          <img src="https://klayswap.com/img/icon/ic-target-swap.svg" />
+          <img
+            src="https://klayswap.com/img/icon/ic-target-swap.svg"
+            alt="swap"
+          />
         </article>
         <KEthCardItem />
       </section>
@@ -39,20 +44,27 @@ const Ticket = () => {
 };
 
 const KlayCardItem = () => {
+  const { myKlayAmount } = useContext(SwapContext);
   return (
     <article className="card-item">
       <label>From</label>
       <article className="exchange">
         <input type="number" placeholder="0" />
-        <img src="https://klayswap.com/img/icon/ic-drp-open-gray.svg" />
+        <img
+          src="https://klayswap.com/img/icon/ic-drp-open-gray.svg"
+          alt="ic-drp-open-gray"
+        />
         <div className="token-img">
-          <img src="https://s.klayswap.com/data/img/token/0x0000000000000000000000000000000000000000/icon.svg?v=1647939876203" />
+          <img
+            src="https://s.klayswap.com/data/img/token/0x0000000000000000000000000000000000000000/icon.svg?v=1647939876203"
+            alt="klay"
+          />
         </div>
       </article>
       <article className="final-part">
         <span className="sub-text">
           <span>보유</span>
-          <span>0</span>
+          <span>{myKlayAmount}</span>
         </span>
         <span className="sub-text2">KLAY</span>
       </article>
@@ -66,9 +78,15 @@ const KEthCardItem = () => {
       <label>To</label>
       <article className="exchange">
         <input type="number" placeholder="0" />
-        <img src="https://klayswap.com/img/icon/ic-drp-open-gray.svg" />
+        <img
+          src="https://klayswap.com/img/icon/ic-drp-open-gray.svg"
+          alt="ic-drp-open-gray"
+        />
         <div className="token-img">
-          <img src="https://s.klayswap.com/data/img/token/0x34d21b1e550d73cee41151c77f3c73359527a396.svg" />
+          <img
+            src="https://s.klayswap.com/data/img/token/0x34d21b1e550d73cee41151c77f3c73359527a396.svg"
+            alt="oEth"
+          />
         </div>
       </article>
       <article className="final-part">
